5a7640b15fb7f4989aba6e19c5deb661919ce751
thddlrla <okjh0999@naver.com>
2023-11-05 23:13:33 +0900

    3-1

---
+++ main.3-1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N_STUDENT  5
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	int a[N_STUDENT] = {1,2,3,4,5};
+	int b[N_STUDENT] = {1,2,3,4,5};
+	int flag = 0;
+	
+	for(i=0; i<N_STUDENT; i++)
+	{
+		if(a[i] == b[i])
+		{
+	    	printf("array a and b are not the same\n");
+	        flag = 1;
+      	}
+      }
+      
+    if (flag==0)
+      printf("array a and b are the same\n");
+      
+    system("PAUSE");
+    
+     
+	return 0;
+}
