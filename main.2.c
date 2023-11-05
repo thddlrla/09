f240bd6d635d85fb7630c53aae317302bbf3df60
thddlrla <okjh0999@naver.com>
2023-11-05 22:52:08 +0900

    Update main.2.c
    09-2

--- main.2.c
+++ main.2.c
@@ -7,12 +7,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
+	int i, average;
+	int sum;
 	int grade[N_STUDENT];
 	printf("input 5 scores : ");
 	
-	for(i=0; i<N_STUDENT;i++)
+	for(i=0; i<N_STUDENT;i++) 
+	{
+	
 	 scanf("%d", &grade[i]);
+	 sum += grade[i];
+    }
+    
+    average = sum/N_STUDENT;
+    
+    printf("score averge: %i\n ", average); 
 	
 	for(i=0; i<N_STUDENT; i++)
 	  printf("grade[%d] = %d\n", i, grade[i]);
