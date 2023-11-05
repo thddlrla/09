37ab9f82167d577031ec5ab1e7fbb27fa5f2c8a0
thddlrla <okjh0999@naver.com>
2023-11-05 22:40:54 +0900

    Create main.2.c

---
+++ main.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define N_STUDENT 5
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i;
+	int grade[N_STUDENT];
+	printf("input 5 scores : ");
+	
+	for(i=0; i<N_STUDENT;i++)
+	 scanf("%d", &grade[i]);
+	
+	for(i=0; i<N_STUDENT; i++)
+	  printf("grade[%d] = %d\n", i, grade[i]);
+	  
+	  
+	system("PAUSE");
+	return 0;
+}
