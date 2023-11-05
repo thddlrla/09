e47b91b25e89e1c19004fcd885960bfd0cae1c6e
thddlrla <okjh0999@naver.com>
2023-11-05 23:47:15 +0900

    Create main.5.c

---
+++ main.5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ROWS 3
+#define COLS 3
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS]);
+{
+	int i;
+	for(i=0; i<Matrix; i++){
+		
+	
+ 	int C[] = int A[] + int B[]
+ }
+}
+
+void printfMatrix(int A[][COLS]);
+{
+	
+}
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	
+  int A[ROWS][COLS] = {
+     {2, 3, 0},
+     {8, 9, 1},
+     {7, 0, 5} };
+   int B[ROWS][COLS] = {
+     {1, 0, 0},
+     {0, 1, 0},
+     {0, 0, 1} };
+     
+    int C[ROWS][COLS]; 
+    addMatrix(A, B, C);
+    printMatrix(C);
+	
+	
+	return 0;
+}
